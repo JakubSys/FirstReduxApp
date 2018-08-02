1e567caec515410b780ebdaf5401c3d81184df6b
JakubSys <jakub.sys@skni.umcs.pl>
2018-08-02 12:53:20 +0200

    commit#1

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+
+    return [
+      { title: 'Javascript: The Good Parts', pages: 101},
+      { title: 'Harry Potter', pages: 200 },
+      { title: 'The Dark Tower', pages: 400 },
+      { title: 'EoquentRuby', pages: 2 }
+
+    ];
+}
